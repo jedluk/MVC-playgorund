7f0405091c09857a182b81acf0a45f0fadc87664
Jedrzej.Lukasiuk <jlukasiuk@teonite.com>
2019-01-02 21:13:35 +0100

    Pug as a template enginge

---
+++ routes/shop.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const path = require('path');
+const ROOT_DIR = require('../util/path');
+const adminData = require('./admin');
+
+router.get('/', (req, res, next) => {
+    // res.sendFile(path.join(ROOT_DIR, 'views', 'shop.html'));
+    const { products } = adminData;
+    res.render('shop', { prods: products, docTitle: 'Shop' });
+});
+
+module.exports = router;
